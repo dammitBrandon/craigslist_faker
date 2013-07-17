ca5c6acecfc0e71228eb6751bc8d517df39ecd48
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 04:37:25 -0500

    unable to configure or properly utilize the foreigner gem, check errors.txt in the app root for the errors when attempting to run rake db:migrate

---
+++ db/migrate/20130717030916_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :name
+      t.text   :description
+      t.string :price
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end

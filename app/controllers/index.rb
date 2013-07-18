ceed41ee1614c6b75d5ba9cee475f17a587b9f6a
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-18 13:18:02 -0500

    dynamic categories listed, working on the adding categories

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,12 +5,13 @@ get '/' do
 end
 
 post '/' do
-  args = {name: params[:post][":name"], description: params[:post][":description"], category_type: params[:post][":category_type"], price: params[:post][":price"]}
+  args = {name: params[:post][":name"], description: params[:post][":description"], category_type: params[:post][":category_type"], price: params[:post][":price"], email: params[:post][":email"]}
   @post = Post.create(args)
   "#{@post}"
 end
 
 get '/post/new' do
+  @categories = Category.all
   erb :new_post
 end
 
@@ -19,5 +20,24 @@ get '/post/success' do
 end
 
 get '/categories/list' do
+  @categories = Category.all
   erb :categories_list
 end
+
+#find a category
+#get that category's posts
+#send the posts array to the erb page
+#  by setting it as an instance variable
+#
+#if @posts.empty?
+#  redirect '/create_p
+#else
+#  erb :another
+#end
+#
+##ON THE HTML PAGE
+#<% if @posts.empty? %>
+#  <h1>NO POSTS<h1>
+#  <% else %>
+#  iterate through posts
+#  <% end %>
--- db/migrate/20130717030916_create_posts.rb
+++ db/migrate/20130717030916_create_posts.rb
@@ -6,6 +6,7 @@ class CreatePosts < ActiveRecord::Migration
       t.string :price
       t.string :email
       t.string :category_type
+      t.references :category
 
       t.timestamps
     end
